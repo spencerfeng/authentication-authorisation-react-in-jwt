3be32107870c08264d23ab63a61ce174be04e87b
Spencer <spencer@sana.com.au>
2017-12-09 17:05:58 +1100

    Create the Redux actions used in user sign up process.

---
+++ client/src/actions.js
@@ -0,0 +1,43 @@
+export const userSignup = (user, history) => {
+    return dispatch => {
+        fetch('http://localhost:3000/signup', {
+            method: 'post',
+            mode: 'cors',
+            headers: {
+                "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"  
+            },
+            body: `firstName=${user.firstName}&lastName=${user.lastName}&email=${user.email}&password=${user.password}`
+        })
+        .then(response => {
+            if (response.status >= 200 && response.status < 300) {
+                return Promise.resolve(response);
+            } else {
+                return Promise.reject(new Error(response.statusText));
+            }
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            if (data.token !== false) {
+                console.log(`response token: ${data.token}`);
+                
+                // Save the token to local storage
+                localStorage.setItem('token', data.token);
+
+                // Update the store state
+                dispatch(setAuthenticatedUser(data.user));
+
+                // Redirect to home page
+                history.push('/');
+            } else {
+                // TODO
+            }
+        })
+        .catch(err => {
+            console.log(err);  
+        });
+    }
+};
+
+export const setAuthenticatedUser = (user) => ({ type: 'SET_USER', data: user });
