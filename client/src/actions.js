84e76c785552409f9c2367c28ea3d2bfea8e442b
Spencer <spencer@sana.com.au>
2017-12-14 20:42:52 +1100

    Create the action to handle user sign in.

--- client/src/actions.js
+++ client/src/actions.js
@@ -40,4 +40,41 @@ export const userSignup = (user, history) => {
     }
 };
 
+export const userLogin = (user, history) => {
+    return dispatch => {
+        fetch('http://localhost:3000/signin', {
+            method: 'post',
+            mode: 'cors',
+            headers: {
+                "Content-type": "application/x-www-form-urlencoded; charset=UTF-8" 
+            },
+            body: `email=${user.email}&password=${user.password}`
+        })
+        .then(response => {
+            if (response.status >= 200 && response.status < 300) {
+                return Promise.resolve(response);
+            } else {
+                return Promise.reject(new Error(response.statusText));
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            if (data.success === true) {
+                // Save the token to local storage
+                localStorage.setItem('token', data.token);
+
+                // Update the store state
+                dispatch(setAuthenticatedUser(data.token));
+
+                // Redirect to user logged-in landing page
+                history.push('/');
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }
+}
+
 export const setAuthenticatedUser = (user) => ({ type: 'SET_USER', data: user });
