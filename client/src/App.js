cab9f545d43645f407e44864211b1a4baee91381
Spencer <spencer@sana.com.au>
2017-12-18 10:24:41 +1100

    Dispatch the ‘authenticateUser’ action in the ‘componentDidMount’ event in the App component.

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,28 @@ import { Route, Switch } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import LoginPage from './components/LoginPageContainer';
 import SignupPage from './components/SignupPageContainer';
+import { authenticateUser } from './actions';
+import { IsEmpty } from './helpers';
 
 class App extends Component {
+
+    componentDidMount() {
+        
+        // check if there is a logged in user
+        // if there is no logged in user, check to see if there is a token item in localstorage
+        // if there is a token item in localstorage, verify if this token is correct and get the its associated user
+        if (IsEmpty(this.props.store.getState().cUser)) {
+    
+            const token = localStorage.getItem('token');
+    
+            if (token !== null) {
+                this.props.store.dispatch(authenticateUser(token));
+            }
+    
+        }
+    
+    }
+
     render() {
         return ( 
             <div className = "App" >
